8ce419337b318ef9e771517e9cb6bf776e7cce7c
Pinae <jonny@pinae.net>
2017-11-14 16:04:55 +0100

    Toggeling fullscreen

--- get_the_funk.py
+++ get_the_funk.py
@@ -8,16 +8,35 @@ class BeamerToLight:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 80*16, 80*9
+        self.size = self.width, self.height = 80*16, 80*9
+        self.fullscreen = False
+
+    def display_window(self):
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+        pygame.display.set_caption("c't Beamer to Light")
+
+    def display_fullscreen(self):
+        self._display_surf = pygame.display.set_mode((0, 0), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN)
 
     def init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.display_window()
         return True
 
     def event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
+        if event.type == pygame.VIDEORESIZE:
+            self.size = self.width, self.height = event.size
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F11:
+                self.fullscreen = not self.fullscreen
+                if self.fullscreen:
+                    self.display_fullscreen()
+                else:
+                    self.display_window()
+            else:
+                print(event)
 
     def loop(self):
         pass
